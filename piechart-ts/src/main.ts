a79aa3c7dd742bd40e32fb6cc7160551e7526f98
Dhamotharan C <dhamotharan.c@hogarth.com>
2024-08-22 22:36:57 +0530

    Highchart view

--- piechart-ts/src/main.ts
+++ piechart-ts/src/main.ts
@@ -1,2 +1,81 @@
 import './style.css'
 
+// import {
+//     ChartConfig,
+//     ChartModel,
+//     Query,
+//     getChartContext,
+//     ChartToTSEvent,
+//     ColumnType,
+//     CustomChartContext,
+//   } from '@thoughtspot/ts-chart-sdk';
+  
+import Highcharts from 'highcharts';
+
+Highcharts.chart('pieApp', {
+    chart: {
+        type: 'pie'
+    },
+    title: {
+        text: 'Unknown'
+    },
+    tooltip: {
+        valueSuffix: '%'
+    },
+    subtitle: {
+        text:
+        'Source: Unknown'
+    },
+    plotOptions: {
+        series: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: [{
+                enabled: true,
+                distance: 20
+            }, {
+                enabled: true,
+                distance: -40,
+                format: '{point.percentage:.1f}%',
+                style: {
+                    fontSize: '1.2em',
+                    textOutline: 'none',
+                    opacity: 0.7
+                },
+                filter: {
+                    operator: '>',
+                    property: 'percentage',
+                    value: 10
+                }
+            }]
+        }
+    },
+    series: [
+        {
+            name: 'Percentage',
+            colorByPoint: true,
+            data: [
+                {
+                    name: 'Water',
+                    y: 55.02
+                },
+                {
+                    name: 'Fat',
+                    y: 26.71
+                },
+                {
+                    name: 'Carbohydrates',
+                    y: 1.09
+                },
+                {
+                    name: 'Protein',
+                    y: 15.5
+                },
+                {
+                    name: 'Ash',
+                    y: 1.68
+                }
+            ]
+        }
+    ]
+} as any);
