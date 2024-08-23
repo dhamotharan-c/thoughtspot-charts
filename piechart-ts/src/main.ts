9e402780386e3069db40c3169a7f5af190da69be
Dhamotharan C <dhamotharan.c@hogarth.com>
2024-08-23 20:06:46 +0530

    pie chart data model

--- piechart-ts/src/main.ts
+++ piechart-ts/src/main.ts
@@ -1,18 +1,63 @@
 import './style.css'
 
-// import {
-//     ChartConfig,
-//     ChartModel,
-//     Query,
-//     getChartContext,
-//     ChartToTSEvent,
-//     ColumnType,
-//     CustomChartContext,
-//   } from '@thoughtspot/ts-chart-sdk';
+import {
+    ChartConfig,
+    ChartModel,
+    Query,
+    getChartContext,
+    ChartToTSEvent,
+    ColumnType,
+    ChartColumn,
+    DataPointsArray,
+    CustomChartContext,
+  } from '@thoughtspot/ts-chart-sdk';
   
 import Highcharts from 'highcharts';
+import 'highcharts/modules/accessibility';
 
-Highcharts.chart('pieApp', {
+import _ from 'lodash';
+
+const getDataModel = (chartModel: ChartModel) =>{
+    const data = chartModel.data;
+
+    const points = data?.[0].data.dataValue.map((row)=>{
+        return{
+            projectname: row[0],
+            tagname: row[1],
+            tagvalue: row[2],
+        };
+    });
+
+    const projectwisepoints = _.groupBy(points,'projectname');
+    const series = Object.keys(projectwisepoints).map((projectName) =>{
+
+        return{
+            name: projectName,
+            data: projectwisepoints[projectName].map((point:any) => {
+                return{
+                    name: point.tagname,
+                    value: point.tagvalue,
+                }
+            }),
+        }
+    
+    });
+    return{
+        series,
+    }
+
+}
+
+
+const renderChart = (ctx: CustomChartContext) => {
+
+    const chartModel = ctx.getChartModel();
+    const dataModel = getDataModel(chartModel);
+
+    Highcharts.chart('pieApp', {
+    accessibility: {
+        enabled: false
+    },
     chart: {
         type: 'pie'
     },
@@ -26,56 +71,97 @@ Highcharts.chart('pieApp', {
         text:
         'Source: Unknown'
     },
-    plotOptions: {
-        series: {
-            allowPointSelect: true,
-            cursor: 'pointer',
-            dataLabels: [{
-                enabled: true,
-                distance: 20
-            }, {
-                enabled: true,
-                distance: -40,
-                format: '{point.percentage:.1f}%',
-                style: {
-                    fontSize: '1.2em',
-                    textOutline: 'none',
-                    opacity: 0.7
-                },
-                filter: {
-                    operator: '>',
-                    property: 'percentage',
-                    value: 10
-                }
-            }]
-        }
-    },
-    series: [
-        {
-            name: 'Percentage',
-            colorByPoint: true,
-            data: [
-                {
-                    name: 'Water',
-                    y: 55.02
-                },
-                {
-                    name: 'Fat',
-                    y: 26.71
-                },
-                {
-                    name: 'Carbohydrates',
-                    y: 1.09
-                },
-                {
-                    name: 'Protein',
-                    y: 15.5
-                },
-                {
-                    name: 'Ash',
-                    y: 1.68
-                }
-            ]
-        }
-    ]
-} as any);
+    series: dataModel.series as any,
+    // series: [
+    //     {
+    //         name: 'Poduct', //Project name - Basically a type of series name column
+    //         colorByPoint: true,
+    //         data: [
+    //             {                        //Two columns struct
+    //                 name: 'Water',       //Column attribute selection
+    //                 y: 55.02             //Column Measure selection
+    //             },
+    //             {
+    //                 name: 'Fat',
+    //                 y: 26.71
+    //             },
+    //             {
+    //                 name: 'Carbohydrates',
+    //                 y: 1.09
+    //             },
+    //             {
+    //                 name: 'Protein',
+    //                 y: 15.5
+    //             },
+    //             {
+    //                 name: 'Ash',
+    //                 y: 1.68
+    //             }
+    //         ]
+    //     }
+    // ]
+    } as any);
+
+    return Promise.resolve();
+}
+
+const init = async () => {
+    const ctx = await getChartContext({
+        getDefaultChartConfig: (chartModel: ChartModel): ChartConfig[] => {
+            const columns = chartModel.columns;
+
+            const measureColumns = _.filter(
+                columns, 
+                (columns) => columns.type === ColumnType.MEASURE,
+            );
+            const attributeColumns = _.filter(
+                columns, 
+                (columns) => columns.type === ColumnType.ATTRIBUTE,
+            );
+            const chartConfig: ChartConfig = {
+                key: 'default',
+                dimensions:[
+                    {
+                        key: 'projectname',
+                        columns: [columns[0]],
+                    },
+                    {
+                        key: 'tagname',
+                        columns: [attributeColumns[1]],
+                    },
+                    {
+                        key: 'tagvalue',
+                        columns: [measureColumns[2]],
+                    },
+                    
+                ],
+            };
+            return [chartConfig]; 
+
+
+        },
+        getQueriesFromChartConfig: (
+            chartConfig: ChartConfig[],
+        ): Array<Query> => {
+            return chartConfig.map(
+                (config: ChartConfig): Query =>
+                    _.reduce(
+                        config.dimensions,
+                        (acc: Query, dimension) => ({
+                            queryColumns: [
+                                ...acc.queryColumns,
+                                ...dimension.columns,
+                            ],
+                        }),
+                        {
+                            queryColumns: [],
+                        } as Query,
+                    ),
+            );
+        },
+        renderChart: (context:CustomChartContext) => renderChart(context),
+    });
+    await renderChart(ctx);
+};
+
+init();
